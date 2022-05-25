dffa2a152f6662a2729ab8428ebb56dcab8453db
derickfelix@zoho.com <derickfelix@zoho.com>
2022-05-24 21:44:28 -0300

    try to track every transaction of previous contract, fail

---
+++ scripts/track_transactions.js
@@ -0,0 +1,19 @@
+const Web3 = require('web3');
+const contractAddress = '0xc78fEABea35EB17CcCd7bDb5A1d2478b19be03c3';
+const abi = `[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]`;
+
+const web3 = new Web3(new Web3.providers.HttpProvider( `https://bsc-dataseed1.binance.org:443`));
+const bdf = new web3.eth.Contract(JSON.parse(abi), contractAddress);
+
+(async () => {
+  // bdf.methods.totalSupply().call({ from: '0xD13b7827B50e201d4A1a0bFC4CaC4A9518D66540' }, (err, res) => {
+  //   console.log(res);
+  // })
+  // console.log(await bdf.methods.totalSupply());
+  const from = 15176743;
+  const results = await bdf.getPastEvents('Transfer',  {
+    fromBlock: 18044606 - 5000,
+    toBlock: 18044606
+  });
+  console.log(results);
+})();
