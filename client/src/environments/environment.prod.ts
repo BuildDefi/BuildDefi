32b8817e0f830636f5f4d92ff2a2302422239011
derickfelix@zoho.com <derickfelix@zoho.com>
2022-06-06 00:17:45 -0300

    feat: update production environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: false,
+  contract: {
+    address: '0x16Ceef356A92605dfd6393f2FD05d4f696fc7E8B'
+  },
+  chain: {
+    id: 56,
+    name: 'Smart Chain'
+  }
 };
