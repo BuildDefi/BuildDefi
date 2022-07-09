cf0e9e36d88ae2c9d0df7c219a5c82225eda2fa8
derickfelix <derickfelix@zoho.com>
2022-07-09 19:27:54 -0300

    feat: add database communication

--- server/routes/customers.js
+++ server/routes/customers.js
@@ -1,8 +1,51 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (_, res) => {
-  res.status(200).send([{ name: 'test' }]);
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: "db-40677.nuvem-us-04.absamcloud.com",
+  port: 27187,
+  user: "crypto",
+  password: "hr2agm0iwc7f",
+  database: 'crypto'
+});
+
+router.get('/', (req, res) => {
+  const query = 'select * from customers';
+
+  db.query(query, (err, data) => {
+    if (err) {
+      res.status(400).json({
+        status: 400,
+        message: err.message
+      });
+      return;
+    }
+
+    res.status(200).send(data);
+  });
+});
+
+router.post('/', (req, res) => {
+  const { registeredName, tradingName, taxPayerRegistration, phoneNumber, email, comments, productService } = req.body;
+  const query = 'INSERT INTO customers(registeredName, tradingName, taxPayerRegistration, phoneNumber, email, comments, productService) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  const values = [registeredName, tradingName, taxPayerRegistration, phoneNumber, email, comments, productService];
+
+  db.query(query, values, (err) => {
+    if (err) {
+      res.status(400).json({
+        status: 400,
+        message: err.message
+      });
+      return;
+    }
+
+    res.json({
+      status: 200,
+      message: 'Success'
+    });
+  });
 });
 
 module.exports = router;
